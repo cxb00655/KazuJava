2071ebc1319c1abfb6f07e943df12e9d15717fec
kazu <cxb00655@gmail.com>
2024-08-04 11:01:53 +0900

    Locale

--- src/local/SampleLocale.java
+++ src/local/SampleLocale.java
@@ -14,8 +14,38 @@ public class SampleLocale {
 		Locale loca_fr_CA = new Locale("fr", "CA");
 		System.out.println("getCountry is  " + loca_fr_CA.getCountry());
 		System.out.println("getLanguage is  " + loca_fr_CA.getLanguage());
-		
 
+		
+		Locale l1  = Locale.CANADA;
+		Locale l2  = Locale.CANADA_FRENCH;
+		Locale l3  = Locale.CHINA;
+		Locale l4  = Locale.CHINESE;
+		Locale l5  = Locale.ENGLISH;
+		Locale l6  = Locale.FRANCE;
+		Locale l7  = Locale.FRENCH;
+		Locale l8  = Locale.GERMAN;
+		Locale l9  = Locale.GERMANY;
+		Locale la  = Locale.ITALIAN;
+		Locale lb  = Locale.ITALY;
+		Locale lc  = Locale.JAPAN;
+		Locale ld  = Locale.JAPANESE;
+		Locale le  = Locale.KOREA;
+		Locale lf  = Locale.KOREAN;
+		Locale lg  = Locale.PRC;
+		Locale lh  = Locale.ROOT;
+		Locale li  = Locale.SIMPLIFIED_CHINESE;
+		Locale lj  = Locale.TAIWAN;
+		Locale lk  = Locale.TRADITIONAL_CHINESE;
+		Locale ll  = Locale.UK;
+		Locale lm  = Locale.US;
+		
+		
+		
+		Locale localJp = new Locale.Builder()
+				.setLanguage("ja")
+				.setRegion("JP")
+				.setScript("Jpan")
+				.build();
 	}
 
 }
